9d75279d81bd45fd8f029d1683fdff092b122dc0
Dzmitry Guzouski <guzich_d@bk.ru>
2022-11-23 17:23:24 +0300

    edit test_items.py

--- test_items.py
+++ test_items.py
@@ -1,9 +1,9 @@
 import time
-
+from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_should_have_button(browser):
     browser.get(link)
     time.sleep(30)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
-    assert browser.find_element_by_css_selector(".btn-add-to-basket").is_displayed()
+    browser.find_element(By.XPATH, "//button[contains(@class,'btn btn-lg btn-primary btn-add-to-basket')]")
+    assert browser.find_element(By.XPATH, "//button[contains(@class,'btn btn-lg btn-primary btn-add-to-basket')]").is_displayed()
